a2e47bfee164c80274b0f66ea7d45b24158a27eb
DESKTOP-HCMJU23\kiustudent <ubilava.anna@kiu.edu.ge>
2025-06-07 13:29:08 +0400

    Revert "changed ports"
    This reverts commit 4dfe0e2f6e307c58399c6d773ccc74a5d6153a39.

--- server/index.ts
+++ server/index.ts
@@ -88,6 +88,4 @@ setInterval(() => {
   io.emit("gameState", gameState);
 }, 1000 / 60);
 
-server.listen(3001, "0.0.0.0", () => {
-  console.log("Server running on port 3001");
-});
+server.listen(3001, () => console.log("Server running on port 3001"));
